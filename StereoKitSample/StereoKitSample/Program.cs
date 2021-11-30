01b844614449e4826da067330e6bc150677db235
HiromuKato <hiromu1212@gmail.com>
2021-12-01 03:44:15 +0900

    日本語コメント追加

--- StereoKitSample/StereoKitSample/Program.cs
+++ StereoKitSample/StereoKitSample/Program.cs
@@ -1,4 +1,4 @@
-using StereoKit;
+﻿using StereoKit;
 using System;
 
 namespace StereoKitSample
@@ -7,36 +7,56 @@ namespace StereoKitSample
     {
         static void Main(string[] args)
         {
-            // Initialize StereoKit
+            // StereoKit の初期化
             SKSettings settings = new SKSettings
             {
+                // アプリ名
                 appName = "StereoKitSample",
+                // アセットフォルダの相対パス（StereoKit はこの配下のアセットを探しに行く）
                 assetsFolder = "Assets",
             };
+
+            // 初期化に失敗した場合はアプリ終了
             if (!SK.Initialize(settings))
+            {
                 Environment.Exit(1);
+            }
 
-
-            // Create assets used by the app
+            // アプリで利用するアセットを生成する
+            // キューブ
             Pose cubePose = new Pose(0, 0, -0.5f, Quat.Identity);
             Model cube = Model.FromMesh(
                 Mesh.GenerateRoundedCube(Vec3.One * 0.1f, 0.02f),
                 Default.MaterialUI);
-
+            // 床
             Matrix floorTransform = Matrix.TS(0, -1.5f, 0, new Vec3(30, 0.1f, 30));
             Material floorMaterial = new Material(Shader.FromFile("floor.hlsl"));
             floorMaterial.Transparency = Transparency.Blend;
 
-
-            // Core application loop
+            // アプリのメインループ
+            // ここのコールバックが入力・システムイベントの後、描画イベントの前に毎フレーム呼ばれる
             while (SK.Step(() =>
             {
-                if (SK.System.displayType == Display.Opaque)
-                    Default.MeshCube.Draw(floorMaterial, floorTransform);
+                // Esc キーでアプリ終了
+                if (Input.Key(Key.Esc).IsJustActive())
+                {
+                    SK.Quit();
+                }
 
+                // ディスプレイタイプが Opaque(VR ヘッドセットや PC) の場合は床を描画する
+                if (SK.System.displayType == Display.Opaque)
+                {
+                    Default.MeshCube.Draw(floorMaterial, floorTransform);
+                }
+
+                // UI クラス：ユーザインターフェースとインタラクションメソッドのコレクション
+                // Handle メソッド：掴んだり移動する機能の開始・終了をハンドルする（掴んでいる間はtrueを返す）
                 UI.Handle("Cube", ref cubePose, cube.Bounds);
+                // キューブを描画する
                 cube.Draw(cubePose.ToMatrix());
             })) ;
+
+            // アプリ終了処理（StereoKit と全てのリソースをクリーンアップする）
             SK.Shutdown();
         }
     }
