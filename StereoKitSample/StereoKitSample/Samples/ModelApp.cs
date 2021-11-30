03b831c8b44d6fa8f0ae26741d469401ab94ad34
HiromuKato <hiromu1212@gmail.com>
2021-12-01 05:48:54 +0900

    3Dモデルを表示するサンプル追加

---
+++ StereoKitSample/StereoKitSample/Samples/ModelApp.cs
@@ -0,0 +1,34 @@
+﻿using StereoKit;
+
+namespace StereoKitSample.Samples
+{
+    /// <summary>
+    /// 3Dモデルを表示するサンプル
+    /// </summary>
+    class ModelApp : IApp
+    {
+        private Model model;
+        private Pose pose;
+        private float scale = 0.1f;
+
+        public void Initialize()
+        {
+            // モデルの読み込み
+            model = Model.FromFile("DamagedHelmet.gltf");
+            pose = new Pose(0, 0, -0.5f, Quat.Identity);
+        }
+
+        public void Update()
+        {
+            // モデルを掴んで移動可能にする
+            UI.Handle("Model", ref pose, model.Bounds * scale);
+
+            // モデルを描画する
+            model.Draw(pose.ToMatrix(scale));
+        }
+
+        public void Shutdown()
+        {
+        }
+    }
+}
