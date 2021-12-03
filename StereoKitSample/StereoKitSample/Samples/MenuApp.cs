0a244275887e7f3299016a1d091d82c4a1fbf004
HiromuKato <hiromu1212@gmail.com>
2021-12-03 21:14:07 +0900

    メニューサンプルにStereoKitで利用可能なUIを追加

--- StereoKitSample/StereoKitSample/Samples/MenuApp.cs
+++ StereoKitSample/StereoKitSample/Samples/MenuApp.cs
@@ -4,15 +4,21 @@ using System.Diagnostics;
 namespace StereoKitSample.Samples
 {
     /// <summary>
-    /// メニューを表示するサンプル
+    /// メニューの表示と StereoKit で利用可能な UI のサンプル
     /// </summary>
     public class MenuApp : IApp
     {
-        static Pose menuPose = new Pose(0.4f, 0, -0.4f, Quat.LookDir(-1, 0, 1));
-        static Sprite appLogo;
+        private Pose menuPose = new Pose(0, 0, -0.5f, Quat.LookDir(0, 0, 1));
+        private Pose menu2Pose = new Pose(0.5f, 0, -0.5f, Quat.LookDir(0, 0, 1));
+        private Sprite appLogo;
+        private string text = "";
+        private float sliderValue = 1;
+        private bool isToggled = false;
+        private int radioBtnNum = 1;
 
         public void Initialize()
         {
+            // 画像の読み込み
             appLogo = Sprite.FromFile("StereoKitWide.png");
         }
 
@@ -20,33 +26,56 @@ namespace StereoKitSample.Samples
         {
             // メニューウィンドウを開始する
             UI.WindowBegin("Menu", ref menuPose, UIWin.Body);
-
-            // 画像を追加
-            UI.Image(appLogo, V.XY(UI.LayoutRemaining.x, 0));
-
-            // ボタンを追加
-            if (UI.Button("Foo"))
             {
-                Debug.WriteLine("Foo");
+                // 画像の追加
+                UI.Image(appLogo, V.XY(UI.LayoutRemaining.x, 0));
+
+                // ボタンの追加
+                if (UI.Button("Foo"))
+                {
+                    text = "Foo";
+                }
+                // 同じ行にボタンを追加
+                UI.SameLine();
+                if (UI.Button("Bar"))
+                {
+                    text = "Bar";
+                }
+                UI.SameLine();
+                // ラベルの追加
+                UI.Label(text);
+
+                UI.Label("Value:" + sliderValue.ToString("F3"));
+                // スライダーの追加
+                UI.HSlider("ScaleSlider", ref sliderValue, 0, 1, 0);
+
+                // トグルボタンの追加
+                UI.Toggle("Toggle Button", ref isToggled);
+
+                // ラジオボタンの追加
+                if (UI.Radio("Radio1", radioBtnNum == 1)) radioBtnNum = 1;
+                UI.SameLine();
+                if (UI.Radio("Radio2", radioBtnNum == 2)) radioBtnNum = 2;
+                UI.SameLine();
+                if (UI.Radio("Radio3", radioBtnNum == 3)) radioBtnNum = 3;
+
+                // セパレーターの追加
+                UI.HSeparator();
+
+                // アプリ終了ボタンの追加
+                if (UI.Button("Quit"))
+                {
+                    SK.Quit();
+                }
             }
-
-            // 同じ行にボタンを追加
-            UI.SameLine();
-            if (UI.Button("Bar"))
-            {
-                Debug.WriteLine("Bar");
-            }
-
-            // セパレーターの追加
-            UI.HSeparator();
-
-            // アプリ終了ボタンの追加
-            if (UI.Button("Quit"))
-            {
-                SK.Quit();
-            }
-
             // メニューウィンドウを終了する
+            UI.WindowEnd();
+
+            // 別のメニュー（ヘッダー付き）を表示する
+            UI.WindowBegin("Menu2", ref menu2Pose, UIWin.Normal);
+            {
+                UI.Label("StereoKit");
+            }
             UI.WindowEnd();
         }
 
