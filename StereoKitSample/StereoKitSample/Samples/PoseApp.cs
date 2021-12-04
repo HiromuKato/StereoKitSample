00b8cea5b5dda0f963207486a17dd081e9ff171b
HiromuKato <hiromu1212@gmail.com>
2021-12-04 18:12:04 +0900

    人差し指のポーズ表示追加

--- StereoKitSample/StereoKitSample/Samples/PoseApp.cs
+++ StereoKitSample/StereoKitSample/Samples/PoseApp.cs
@@ -3,7 +3,7 @@
 namespace StereoKitSample.Samples
 {
     /// <summary>
-    /// ヘッド、ハンド、オブジェクトの位置・前方ベクトル、マウスの位置を表示するサンプル
+    /// ヘッド、ハンド、人差し指、オブジェクトの位置・前方ベクトル、マウスの位置を表示するサンプル
     /// </summary>
     public class PoseApp : IApp
     {
@@ -15,7 +15,6 @@ namespace StereoKitSample.Samples
 
         public void Update()
         {
-            // 別のメニュー（ヘッダー付き）を表示する
             UI.WindowBegin("Position / Forward", ref menuPose, UIWin.Normal);
             {
                 // 頭のポーズ表示
@@ -26,6 +25,10 @@ namespace StereoKitSample.Samples
                 Hand handL = Input.Hand(Handed.Left);
                 UI.Label("Right Hand(palm): " + handR.palm);
                 UI.Label("Left Hand(palm): " + handL.palm);
+
+                // 右手人差し指のポーズ表示
+                HandJoint index = handR[FingerId.Index, JointId.Tip];
+                UI.Label("Index: " + index.Pose);
 
                 // オブジェクト（メニュー）のポーズ表示
                 UI.Label("Menu: " + menuPose);
