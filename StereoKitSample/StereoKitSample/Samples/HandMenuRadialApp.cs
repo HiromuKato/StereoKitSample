8129e963a51b9e5dca4eb747bb02c71bdbfd0efa
HiromuKato <hiromu1212@gmail.com>
2021-12-05 03:46:05 +0900

    ラジアルハンドメニューのサンプル追加

---
+++ StereoKitSample/StereoKitSample/Samples/HandMenuRadialApp.cs
@@ -0,0 +1,47 @@
+﻿using StereoKit;
+using StereoKit.Framework;
+
+namespace StereoKitSample.Samples
+{
+    /// <summary>
+    /// ラジアルハンドメニューのサンプル
+    /// </summary>
+    public class HandMenuRadialApp : IApp
+    {
+        HandMenuRadial handMenu;
+        private string text = "HandMenuRadial Sample";
+        private Pose textPose;
+
+        public void Initialize()
+        {
+            handMenu = SK.AddStepper(new HandMenuRadial(
+                new HandRadialLayer("Root",
+                    new HandMenuItem("File", null, () => text = "File", "File"),
+                    new HandMenuItem("Edit", null, () => text = "Edit", "Edit"),
+                    new HandMenuItem("About", null, () => text = "StereoKit " + SK.VersionName),
+                    new HandMenuItem("Cancel", null, () => text = "Cancel")),
+                new HandRadialLayer("File",
+                    new HandMenuItem("New", null, () => text = "New"),
+                    new HandMenuItem("Open", null, () => text = "Open"),
+                    new HandMenuItem("Close", null, () => text = "Close"),
+                    new HandMenuItem("Back", null, () => text = "Back", HandMenuAction.Back)),
+                new HandRadialLayer("Edit",
+                    new HandMenuItem("Copy", null, () => text = "Copy"),
+                    new HandMenuItem("Paste", null, () => text = "Paste"),
+                    new HandMenuItem("Back", null, () => text = "Back", HandMenuAction.Back))));
+        }
+
+        public void Update()
+        {
+            // テキスト表示
+            textPose.position = Input.Head.position + Input.Head.Forward * 1;
+            textPose.orientation = Quat.LookAt(textPose.position, Input.Head.position);
+            Text.Add(text, textPose.ToMatrix(1), TextAlign.CenterLeft, TextAlign.CenterLeft);
+        }
+
+        public void Shutdown()
+        {
+            SK.RemoveStepper(handMenu);
+        }
+    }
+}
