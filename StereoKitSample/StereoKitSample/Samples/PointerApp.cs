16c1894d94ab55a5b4663c173ef5f062ea3845c8
HiromuKato <hiromu1212@gmail.com>
2021-12-05 17:45:38 +0900

    ポインターのサンプル追加

---
+++ StereoKitSample/StereoKitSample/Samples/PointerApp.cs
@@ -0,0 +1,39 @@
+﻿using StereoKit;
+
+namespace StereoKitSample.Samples
+{
+    /// <summary>
+    /// ポインターのサンプル
+    /// </summary>
+    public class PointerApp : IApp
+    {
+        public void Initialize()
+        {
+        }
+
+        public void Update()
+        {
+            DrawHandPointers();
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        /// <summary>
+        /// 手からポインターを表示する
+        /// </summary>
+        private void DrawHandPointers()
+        {
+            int hands = Input.PointerCount(InputSource.Hand);
+            for (int i = 0; i < hands; i++)
+            {
+                Pointer pointer = Input.Pointer(i, InputSource.Hand);
+                // 長さ 50cm, 太さ1cm の線を描画する
+                Lines.Add(pointer.ray, 0.5f, Color.White, Units.mm2m);
+                // ポインターのポーズを示す座標軸を描画する
+                Lines.AddAxis(pointer.Pose);
+            }
+        }
+    }
+}
