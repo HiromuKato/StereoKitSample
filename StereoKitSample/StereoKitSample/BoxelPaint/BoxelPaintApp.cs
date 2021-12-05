6cb962e8128dba9ee905a715d052c6df7207eab6
HiromuKato <hiromu1212@gmail.com>
2021-12-06 05:45:27 +0900

    キューブの動的生成

--- StereoKitSample/StereoKitSample/BoxelPaint/BoxelPaintApp.cs
+++ StereoKitSample/StereoKitSample/BoxelPaint/BoxelPaintApp.cs
@@ -1,19 +1,31 @@
-﻿using StereoKit;
+﻿using System.Collections.Generic;
+using StereoKit;
 
 namespace StereoKitSample.BoxelPaint
 {
     class BoxelPaintApp : IApp
     {
+        struct CubeData
+        {
+            public Pose pose;
+            public Material material;
+            public Mesh mesh;
+        }
+
         // 全体
         Pose _pose = new Pose(0, 0, -0.6f, Quat.Identity);
 
         // メニュー
         private Pose menuPose = new Pose(0, 0, -0.5f, Quat.LookDir(0, 0, 1));
         private float sliderValue = 8;
+        private Color selectedColor = new Color(1, 1, 1, 1);
 
         // グリッド
         private Color gridColor = new Color(0.2f, 0.2f, 0.2f, 0.5f);
         private int gridMaxCount = 8;
+
+        // 描画内容を保持したリスト
+        private List<CubeData> cubeData = new List<CubeData>();
 
         public void Initialize()
         {
@@ -33,6 +45,12 @@ namespace StereoKitSample.BoxelPaint
                 DrawGrid();
             }
             UI.HandleEnd();
+
+            // キューブの描画
+            foreach (var data in cubeData)
+            {
+                data.mesh.Draw(data.material, data.pose.ToMatrix(1));
+            }
         }
 
         public void Shutdown()
@@ -50,12 +68,36 @@ namespace StereoKitSample.BoxelPaint
                 // ボタンの追加
                 if (UI.Button("Save"))
                 {
+                    selectedColor = new Color(1, 0, 0, 1);
                 }
                 // 同じ行にボタンを追加
                 UI.SameLine();
                 if (UI.Button("Load"))
                 {
+                    selectedColor = new Color(0, 0, 1, 1);
                 }
+
+                UI.SameLine();
+                if (UI.Button("Add"))
+                {
+                    // 右手人差し指の位置取得
+                    Pose indexPose = Input.Hand(Handed.Right)[FingerId.Index, JointId.Tip].Pose;
+
+                    // Cubeメッシュ生成
+                    var mesh = Mesh.GenerateCube(Vec3.One * 5 * U.cm);
+                    var data = new CubeData();
+                    data.pose = indexPose;
+                    var colorMat = Default.Material.Copy();
+                    colorMat[MatParamName.ColorTint] = selectedColor;
+                    data.material = colorMat;
+                    data.mesh = mesh;
+                    cubeData.Add(data);
+                }
+                if (UI.Button("Clear"))
+                {
+                    cubeData.Clear();
+                }
+
 
                 // スライダーの追加
                 UI.Label("Grid", V.XY(5 * U.cm, UI.LineHeight));
